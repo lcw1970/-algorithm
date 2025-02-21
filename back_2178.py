018db3c77da31569e3bd1a6113657f5f0b50e3b0
이재현 <lcw7010@naver.com>
2025-02-21 23:29:12 +0900

    미로찾기

---
+++ back_2178.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n, m = map(int,input().split())
+graph = []
+visit = [[False] * m for _ in range(n)]
+
+for _ in range(n) :
+    data = list(map(int,input()))
+    graph.append(data)
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y) :
+    
+    visit[x][y] = True
+    q = deque()
+    q.append((x,y))
+    while q :
+        cx,cy = q.popleft()
+        for i in range(4):
+            nx,ny = cx + dx[i],cy + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and not visit[nx][ny] and graph[nx][ny] == 1:
+                visit[nx][ny] = True  # 방문 처리
+                q.append((nx, ny))  # 큐에 좌표 추가
+        
+if visit[n-1][m-1] :
+    print("가능")
+else :
+    print("불가능")
