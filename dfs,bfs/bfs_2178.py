2fe40eaa7feb67ef9aaf3b7993b1a7f51dd9fb3f
이재현 <lcw7010@naver.com>
2025-02-24 20:12:42 +0900

    2178번 수정

--- dfs,bfs/bfs_2178.py
+++ dfs,bfs/bfs_2178.py
@@ -12,7 +12,6 @@ dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 def bfs(x,y) :
-    
     visit[x][y] = True
     q = deque()
     q.append((x,y))
@@ -22,10 +21,8 @@ def bfs(x,y) :
             nx,ny = cx + dx[i],cy + dy[i]
             if 0 <= nx < n and 0 <= ny < m and not visit[nx][ny] and graph[nx][ny] == 1:
                 visit[nx][ny] = True  # 방문 처리
+                graph[nx][ny] = graph[cx][cy] +1
                 q.append((nx, ny))  # 큐에 좌표 추가
         
 bfs(0,0)
-if visit[n-1][m-1] :
-    print("가능")
-else :
-    print("불가능")
+print(graph[n-1][m-1]) 
