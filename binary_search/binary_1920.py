c68780f42e588d14a90bf6aba34ae9fb6a371a5d
이재현 <lcw7010@naver.com>
2025-03-05 16:43:56 +0900

    불 문제 커밋 수정필요

--- binary_search/binary_1920.py
+++ binary_search/binary_1920.py
@@ -1,20 +1,24 @@
-def binary(array,target,start,end) :
-    while start <= end :
-        mid = (start + end) //2 
-        if array[mid] == target :
-            return 1
-        elif array[mid] > target :
-            end = mid-1
-        else :
-            start = mid +1
-        
-    return 0
+import sys
+input = sys.stdin.read
+input_data = input().split()
 
-n = int(input())
-n_lst= list(map(int,input().split()))
-m = int(input())
-m_lst = list(map(int,input().split()))
+n = int(input_data[0])
+n_lst = list(map(int, input_data[1:n + 1]))
+m = int(input_data[n + 1])
+m_lst = list(map(int, input_data[n + 2:]))
 
 n_lst.sort()
-for i in m_lst :
-    print(binary(n_lst,i,0,n-1))
+
+def binary(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return 1
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return 0
+
+for i in m_lst:
+    print(binary(n_lst, i, 0, n - 1))
