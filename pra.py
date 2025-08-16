2835c3310af8ff59ea458094ff81f1bd726c40c4
이재현 <lcw7010@naver.com>
2025-08-16 16:59:11 +0900

    오늘 알고리즘

--- pra.py
+++ pra.py
@@ -1,17 +1,9 @@
-# 1 2 3 4 5 6
-# 1 3 6 10 15 21
-# 1 4 10 20 35 
-# 1 5 15 35 70
-t = int(input())
-for _ in range(t):
-    k = int(input())
-    n = int(input())
+n = int(input())
+lst= []
+for _ in range(n):
+    age,name = input().split()
+    lst.append((age,name))
+sort_lst = sorted(lst,key=lambda x:int(x[0]))
 
-    zero = [i for i in range(1,n+1)]
-    for _ in range(k) :
-        value = 0
-        for j in range(n) :
-            value += zero[j]
-            zero[j] = value
-        
-    print(zero[n-1])
+for age,name in sort_lst :
+    print(f"{age} {name}")
